2836c4298b1e1bec7be842c06bb7f7f0d0e73f92
DidacClaria <dclariah99@gmail.com>
2021-11-22 19:41:54 +0100

    added neighbor highlitning and file reading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
 
   def index
+    nodesFile = File.read('public/naziNodes.json')
+    @nazisNodes = JSON.parse(nodesFile)
+
+    linksFile = File.read('public/naziLinks.json')
+    @nazisLinks = JSON.parse(linksFile)
+
   end
 
 end
