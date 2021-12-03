9a74137b8e96c2d1b1377232479a7f89d5f7d58d
DidacClaria <dclariah99@gmail.com>
2021-12-03 20:51:18 +0100

    added favicon, author nodes and edges, settings and cardinfo customization WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::Base
 
   def index
-    nodesFile = File.read('public/naziNodes.json')
+    nodesFile = File.read('public/data/naziNodes.json')
     @nazisNodes = JSON.parse(nodesFile)
 
-    nodesGCCFile = File.read('public/naziNodesGCC.json')
+    subjList = @nazisNodes.flat_map { |node| node["Subjectivity Level"].to_f } 
+
+    @maxSentiment = subjList.max
+
+    nodesGCCFile = File.read('public/data/naziNodesGCC.json')
     @nazisNodesGCC = JSON.parse(nodesGCCFile)
 
-    linksFile = File.read('public/naziLinks.json')
+    linksFile = File.read('public/data/naziLinks.json')
     @nazisLinks = JSON.parse(linksFile)
+
+    authorNodesFile = File.read('public/data/authorNodes.json')
+    @authorNodes = JSON.parse(authorNodesFile)
+
+    authorLinksFile = File.read('public/data/authorEdges.json')
+    @authorLinks = JSON.parse(authorLinksFile)
 
   end
 
