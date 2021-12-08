001e943f5bfb8b5d67266f8bb045ec383e0f4b78
DidacClaria <dclariah99@gmail.com>
2021-12-08 20:41:16 +0100

    correct node values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,19 @@ class ApplicationController < ActionController::Base
     nodesFile = File.read('public/data/naziNodes.json')
     @nazisNodes = JSON.parse(nodesFile)
 
-    subjList = @nazisNodes.flat_map { |node| node["Subjectivity Level"].to_f } 
-
-    @maxSubjectivity = subjList.max
-
-    nodesGCCFile = File.read('public/data/naziNodesGCC.json')
-    @nazisNodesGCC = JSON.parse(nodesGCCFile)
+    subjListDe = @nazisNodes.flat_map { |node| node["Subjectivity de"].to_f } 
+    @maxSubjectivityDe = subjListDe.max
+    subjListIt = @nazisNodes.flat_map { |node| node["Subjectivity it"].to_f } 
+    @maxSubjectivityIt = subjListIt.max
+    subjListPl = @nazisNodes.flat_map { |node| node["Subjectivity pl"].to_f } 
+    @maxSubjectivityPl = subjListPl.max
+    subjListFr = @nazisNodes.flat_map { |node| node["Subjectivity fr"].to_f } 
+    @maxSubjectivityFr = subjListFr.max
+    subjListEn = @nazisNodes.flat_map { |node| node["Subjectivity en"].to_f } 
+    @maxSubjectivityEn = subjListEn.max
 
     linksFile = File.read('public/data/naziLinks.json')
     @nazisLinks = JSON.parse(linksFile)
-
-    authorNodesFile = File.read('public/data/authorNodes.json')
-    @authorNodes = JSON.parse(authorNodesFile)
-
-    authorLinksFile = File.read('public/data/authorEdges.json')
-    @authorLinks = JSON.parse(authorLinksFile)
 
   end
 
